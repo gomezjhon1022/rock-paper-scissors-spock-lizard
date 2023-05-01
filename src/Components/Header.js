38484b31061aa2f2173192da2f057cee8443dcc5
Jhon Gomez <jhonjairogomez92@gmail.com>
2023-05-01 11:14:19 -0500

    Add header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,30 +1,35 @@
 import "./Header.css";
 import {Modal} from "./Modal";
-import {useState} from 'react';
+import React, {useState} from 'react';
 
 function Header (){
 
   const [isOpenModal, setOpenModal] = useState("true");
+  const [score, setScore] =useState(0);
+
+  React.useEffect(() => console.log("se actualiza open modal"), [isOpenModal]);
   return (
-    <div className="header">
-      {!isOpenModal && (<><div className="header-left">
-        ROCK PAPER SCISSORS LIZARD SPOCK
-      </div><div className="header-right">
-          SCORE 13
-        </div></>)}
-      {isOpenModal && <Modal>
+    <>
+      {!isOpenModal &&(<div className="header__container">
+        <div className="header">
+          <div className="header-left">
+          ROCK<br/> PAPER<br/>  SCISSORS<br/>  LIZARD <br/> SPOCK<br/>
+          </div>
+          <div className="header-right">
+            SCORE <br/>
+            <div className="header-right-score">{score}</div>
+          </div>
+        </div>
+        </div>
+        )
+      }
+
+      
+      {isOpenModal && <Modal setOpenModal={setOpenModal} isOpenModal={isOpenModal}>
 
       </Modal>}
-    </div>
+    </>
   )
 }
 
 export default Header
-
-
-// {isOpenModal && (<div className="header-left">
-// ROCK PAPER SCISSORS LIZARD SPOCK
-// </div>
-// <div className="header-right">
-// SCORE 13
-// </div>)}
--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -1,14 +1,13 @@
 import "./Modal.css"
 
 
-function Modal () {
+function Modal ({setOpenModal, isOpenModal}) {
   return (
     <div className="ModalContainer">
       <h3 className="Title">RULES</h3>
       <div className="ModalBackground">
       </div>
-      <button className="ModalButton"></button>
-      <img src="/images/image-rules.svg"/>
+      <button className="ModalButton" onClick={() => setOpenModal(!isOpenModal)}></button>
     </div>
   )
 
