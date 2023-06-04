fbd00f974e2d9d247a1fd9a85f38f3d3224cc196
Jhon Gomez <jhonjairogomez92@gmail.com>
2023-06-03 21:06:49 -0500

    Add election

--- src/App.js
+++ src/App.js
@@ -10,13 +10,13 @@ function App() {
   const [player1Chose, setPlayer1Chose]= useState("false");
 
   useEffect(() => console.log("se actualiza open modal"), [isOpenModal, player1]);
-
+  console.log(player1);
 
   return (
     <div className="App">
       <Header isOpenModal={isOpenModal} setOpenModal={setOpenModal} score={score} setScore={setScore}>
       </Header>
-      <Board isOpenModal={isOpenModal} setOpenModal={setOpenModal} player1={player1} setPlayer1={setPlayer1}>
+      <Board isOpenModal={isOpenModal} setOpenModal={setOpenModal} player1={player1} setPlayer1={setPlayer1} player1Chose={player1Chose} setPlayer1Chose={setPlayer1Chose}>
       </Board>
     </div>
   );
--- src/Components/Board.js
+++ src/Components/Board.js
@@ -1,23 +1,44 @@
 import "./Board.css"
 import React from "react";
 
-function Board({isOpenModal, setOpenModal, setPlayer1}) {
+function Board({isOpenModal, setOpenModal, setPlayer1, player1, setPlayer1Chose, player1Chose}) {
+  console.log(player1, "esto se elige")
   return (
   <>
-    {!isOpenModal &&(<>
+    {!isOpenModal &&player1Chose&&(<>
       <div className="board_container">
         <div className="board">
-          <button className="scissors circle" onClick={()=> setPlayer1("scissors")}></button>
-          <button className="spock circle" onClick={()=> setPlayer1("spock")}></button>
-          <button className="paper circle" onClick={()=> setPlayer1("paper")}></button>
-          <button className="lizard circle" onClick={()=> setPlayer1("lizard")}></button>
-          <button className="rock circle" onClick={()=> setPlayer1("rock")}></button>
+          <button className="scissors circle" onClick={()=> {setPlayer1("scissors-picked");setPlayer1Chose(!player1Chose)}}></button>
+          <button className="spock circle" onClick={()=> {setPlayer1("spock-picked");setPlayer1Chose(!player1Chose)}}></button>
+          <button className="paper circle" onClick={()=> {setPlayer1("paper-picked");setPlayer1Chose(!player1Chose)}}></button>
+          <button className="lizard circle" onClick={()=> {setPlayer1("lizard-picked");setPlayer1Chose(!player1Chose)}}></button>
+          <button className="rock circle" onClick={()=> {setPlayer1("rock-picked");setPlayer1Chose(!player1Chose)}}></button>
         </div>
       </div>
+      </>
+    )}
+    {!player1Chose&&(
+      <div className="board_container">
+        <div className="board_election">
+          <div className="title_election_container">
+            <div className="title_election">YOU PICKED</div>
+            <div className="title_election">THE HOUSE PICKED</div>
+          </div>
+          <div className="options_container">
+            <div className={`circle-picked ${player1}`}></div>
+            <div className="circle_picked_random"></div>
+          </div>
+        </div>
+      </div>
+    )
+
+    }
+
+
+    {!isOpenModal&&(
       <button className="rules" onClick={()=> setOpenModal(!isOpenModal)} >
         <p>RULES</p>
       </button>
-      </>
     )}
   </>
 )}
