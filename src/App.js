6fdb74707fb5409160c65ef94ce71b9d08af4ef7
Jhon Gomez <jhonjairogomez92@gmail.com>
2023-07-09 20:05:25 +0200

    Add election player 2

--- src/App.js
+++ src/App.js
@@ -8,15 +8,18 @@ function App() {
   const [score, setScore] =useState(0);
   const [player1, setPlayer1] =useState("");
   const [player1Chose, setPlayer1Chose]= useState("false");
+  const [player2, setPlayer2]= useState("");
+  const [player2Chose, setPlayer2Chose]= useState("false");
 
   useEffect(() => console.log("se actualiza open modal"), [isOpenModal, player1]);
   console.log(player1);
+  console.log(player2);
 
   return (
     <div className="App">
       <Header isOpenModal={isOpenModal} setOpenModal={setOpenModal} score={score} setScore={setScore}>
       </Header>
-      <Board isOpenModal={isOpenModal} setOpenModal={setOpenModal} player1={player1} setPlayer1={setPlayer1} player1Chose={player1Chose} setPlayer1Chose={setPlayer1Chose}>
+      <Board isOpenModal={isOpenModal} setOpenModal={setOpenModal} player1={player1} setPlayer1={setPlayer1} player1Chose={player1Chose} setPlayer1Chose={setPlayer1Chose} player2={player2} setPlayer2={setPlayer2} player2Chose={player2Chose} setPlayer2Chose={setPlayer2Chose}>
       </Board>
     </div>
   );
--- src/Components/Board.js
+++ src/Components/Board.js
@@ -1,14 +1,38 @@
 import "./Board.css"
 import React from "react";
 
-function Board({isOpenModal, setOpenModal, setPlayer1, player1, setPlayer1Chose, player1Chose}) {
-  console.log(player1, "esto se elige")
+function Board({isOpenModal, setOpenModal, setPlayer1, player1, setPlayer1Chose, player1Chose, player2, setPlayer2, player2Chose, setPlayer2Chose}) {
+  function pickedRandom () {
+    const randomNumber = Math.floor(Math.random()*5);
+    switch (randomNumber) {
+      case 0:
+        setPlayer2("scissors-picked");
+        setPlayer2Chose(!player2Chose);
+        break;
+      case 1:
+        setPlayer2("spock-picked");
+        setPlayer2Chose(!player2Chose);
+        break;
+      case 2:
+        setPlayer2("paper-picked");
+        setPlayer2Chose(!player2Chose);
+        break;
+      case 3:
+        setPlayer2("lizard-picked");
+        setPlayer2Chose(!player2Chose);
+        break;
+      default:
+        setPlayer2("rock-picked");
+        setPlayer2Chose(!player2Chose);
+    }
+  }
+
   return (
   <>
     {!isOpenModal &&player1Chose&&(<>
       <div className="board_container">
         <div className="board">
-          <button className="scissors circle" onClick={()=> {setPlayer1("scissors-picked");setPlayer1Chose(!player1Chose)}}></button>
+          <button className="scissors circle" onClick={()=> {setPlayer1("scissors-picked");setPlayer1Chose(!player1Chose);setTimeout (pickedRandom,2000)}}></button>
           <button className="spock circle" onClick={()=> {setPlayer1("spock-picked");setPlayer1Chose(!player1Chose)}}></button>
           <button className="paper circle" onClick={()=> {setPlayer1("paper-picked");setPlayer1Chose(!player1Chose)}}></button>
           <button className="lizard circle" onClick={()=> {setPlayer1("lizard-picked");setPlayer1Chose(!player1Chose)}}></button>
@@ -26,7 +50,8 @@ function Board({isOpenModal, setOpenModal, setPlayer1, player1, setPlayer1Chose,
           </div>
           <div className="options_container">
             <div className={`circle-picked ${player1}`}></div>
-            <div className="circle_picked_random"></div>
+            {player2Chose&&<div className={`circle_picked_random ${player2}`}></div>}
+            {!player2Chose&&<div className={`circle_picked_random_chose ${player2}`}></div>}
           </div>
         </div>
       </div>
