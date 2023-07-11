33a95d9b2e80b5ace99019ddfb5e4816de93ff20
Jhon Gomez <jhonjairogomez92@gmail.com>
2023-07-11 20:35:53 +0200

    add game logic

--- src/App.js
+++ src/App.js
@@ -10,16 +10,26 @@ function App() {
   const [player1Chose, setPlayer1Chose]= useState("false");
   const [player2, setPlayer2]= useState("");
   const [player2Chose, setPlayer2Chose]= useState("false");
+  const [player1IsWinner, setPlayer1IsWinner] =useState("false");
+  const [player2IsWinner, setPlayer2IsWinner] =useState("false");
+  const [isDraw, setIsDraw] = useState("false");
 
-  useEffect(() => console.log("se actualiza open modal"), [isOpenModal, player1]);
-  console.log(player1);
-  console.log(player2);
+  useEffect(()=>{
+    setTimeout(()=>{
+      if (player1 === player2) {
+        setIsDraw(!isDraw)}
+      else if ((player1==="rock-picked"&& player2==="scissors-picked")||(player1==="scissors-picked"&& player2==="paper-picked")||(player1==="paper-picked"&& player2==="rock-picked")||(player1==="paper-picked"&& player2==="spock-picked")||(player1==="spock-picked"&& player2==="scissors-picked")||(player1==="scissors-picked"&& player2==="lizard-picked")||(player1==="lizard-picked"&& player2==="spock-picked")||(player1==="spock-picked"&& player2==="rock-picked")||(player1==="rock-picked"&& player2==="lizard-picked")||(player1==="lizard-picked"&& player2==="paper-picked")) {setPlayer1IsWinner(!player1IsWinner);setScore(score+1)
+      }
+      else {setPlayer2IsWinner(!player2IsWinner)}
+
+    },1000)
+  },[player2])
 
   return (
     <div className="App">
       <Header isOpenModal={isOpenModal} setOpenModal={setOpenModal} score={score} setScore={setScore}>
       </Header>
-      <Board isOpenModal={isOpenModal} setOpenModal={setOpenModal} player1={player1} setPlayer1={setPlayer1} player1Chose={player1Chose} setPlayer1Chose={setPlayer1Chose} player2={player2} setPlayer2={setPlayer2} player2Chose={player2Chose} setPlayer2Chose={setPlayer2Chose}>
+      <Board isOpenModal={isOpenModal} setOpenModal={setOpenModal} player1={player1} setPlayer1={setPlayer1} player1Chose={player1Chose} setPlayer1Chose={setPlayer1Chose} player2={player2} setPlayer2={setPlayer2} player2Chose={player2Chose} setPlayer2Chose={setPlayer2Chose} player1IsWinner={player1IsWinner} setPlayer1IsWinner={setPlayer1IsWinner} player2IsWinner={player2IsWinner} setPlayer2IsWinner={setPlayer2IsWinner} isDraw={isDraw} setIsDraw={setIsDraw}>
       </Board>
     </div>
   );
--- src/Components/Board.js
+++ src/Components/Board.js
@@ -1,7 +1,7 @@
 import "./Board.css"
 import React from "react";
 
-function Board({isOpenModal, setOpenModal, setPlayer1, player1, setPlayer1Chose, player1Chose, player2, setPlayer2, player2Chose, setPlayer2Chose}) {
+function Board({isOpenModal, setOpenModal, setPlayer1, player1, setPlayer1Chose, player1Chose, player2, setPlayer2, player2Chose, setPlayer2Chose, player1IsWinner, setPlayer1IsWinner, player2IsWinner, setPlayer2IsWinner, isDraw, setIsDraw}) {
   function pickedRandom () {
     const randomNumber = Math.floor(Math.random()*5);
     switch (randomNumber) {
@@ -27,16 +27,26 @@ function Board({isOpenModal, setOpenModal, setPlayer1, player1, setPlayer1Chose,
     }
   }
 
+function playAgain() {
+  setPlayer1Chose("false");
+  setPlayer2Chose("false");
+  setIsDraw("false");
+  setPlayer1IsWinner("false");
+  setPlayer2IsWinner("false");
+  setPlayer1("");
+  setPlayer2("");
+}
+
   return (
   <>
     {!isOpenModal &&player1Chose&&(<>
       <div className="board_container">
         <div className="board">
-          <button className="scissors circle" onClick={()=> {setPlayer1("scissors-picked");setPlayer1Chose(!player1Chose);setTimeout (pickedRandom,2000)}}></button>
-          <button className="spock circle" onClick={()=> {setPlayer1("spock-picked");setPlayer1Chose(!player1Chose)}}></button>
-          <button className="paper circle" onClick={()=> {setPlayer1("paper-picked");setPlayer1Chose(!player1Chose)}}></button>
-          <button className="lizard circle" onClick={()=> {setPlayer1("lizard-picked");setPlayer1Chose(!player1Chose)}}></button>
-          <button className="rock circle" onClick={()=> {setPlayer1("rock-picked");setPlayer1Chose(!player1Chose)}}></button>
+          <button className="scissors circle" onClick={()=> {setPlayer1("scissors-picked");setPlayer1Chose(!player1Chose);setTimeout (pickedRandom,1000)}}></button>
+          <button className="spock circle" onClick={()=> {setPlayer1("spock-picked");setPlayer1Chose(!player1Chose);setTimeout (pickedRandom,1000)}}></button>
+          <button className="paper circle" onClick={()=> {setPlayer1("paper-picked");setPlayer1Chose(!player1Chose);setTimeout (pickedRandom,1000)}}></button>
+          <button className="lizard circle" onClick={()=> {setPlayer1("lizard-picked");setPlayer1Chose(!player1Chose);setTimeout (pickedRandom,1000)}}></button>
+          <button className="rock circle" onClick={()=> {setPlayer1("rock-picked");setPlayer1Chose(!player1Chose);setTimeout (pickedRandom,1000)}}></button>
         </div>
       </div>
       </>
@@ -50,15 +60,25 @@ function Board({isOpenModal, setOpenModal, setPlayer1, player1, setPlayer1Chose,
           </div>
           <div className="options_container">
             <div className={`circle-picked ${player1}`}></div>
+            {isDraw&&<div className="button_winner">
+            <p>DRAW</p>
+            <button className="button_play_again"onClick={()=>playAgain()}>PLAY AGAIN</button>
+            </div>}
+            {!player1IsWinner&&<div className="button_winner">
+              <p>YOU WIN</p>
+              <button className="button_play_again" onClick={()=>playAgain()}>PLAY AGAIN</button>
+            </div>}
+            {!player2IsWinner&&<div className="button_winner">
+              <p>YOU LOSE</p>
+              <button className="button_play_again" onClick={()=>playAgain()}>PLAY AGAIN</button>
+            </div>}
             {player2Chose&&<div className={`circle_picked_random ${player2}`}></div>}
             {!player2Chose&&<div className={`circle_picked_random_chose ${player2}`}></div>}
           </div>
         </div>
       </div>
     )
-
     }
-
 
     {!isOpenModal&&(
       <button className="rules" onClick={()=> setOpenModal(!isOpenModal)} >
